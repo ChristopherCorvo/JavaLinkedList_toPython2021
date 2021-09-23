512478e14efe315d3dfafd5c2bfe86a98965f088
Christopher Corvo <christophercorvo@gmail.com>
2021-09-23 18:09:18 -0400

    adds initial python files

---
+++ .idea/LinkedListClass/LinkedListClass.py
@@ -0,0 +1,8 @@
+'''
+    Goal: For this practice session I will be attempting to translate the Java LinkedList Class into python.
+
+    The Java LinkedList Class that I will be using as my template for this python class was written by the programmer
+    Josh Bloch
+
+    To see the Java LinkedList Class included in the Java Language visit this link:
+'''
